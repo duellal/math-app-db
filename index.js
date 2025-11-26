a3c93e57e3c8eef95e0aa6f330ce97e6848713dc
Alex <alexandriaduell@gmail.com>
2025-11-26 17:42:59 -0500

    Added index.js init code

---
+++ index.js
@@ -0,0 +1,26 @@
+// Imports
+const express = require('express')
+const cors = require('cors')
+
+// Endpoint Imports
+
+const middleware = [cors(), express.json()]
+
+const app = express()
+const PORT = process.env.PORT || 8001
+
+app.use(middleware)
+
+// Server connection
+app.listen(PORT, async () => {
+    console.log(`Server is running on port ${PORT}`)
+})
+
+// Health endpoint:
+app.get('/api/health', (req, res) => {
+    res.status(200).json({
+        message: 'Math Problem Generator - Health check point complete',
+    })
+})
+
+// Routes
