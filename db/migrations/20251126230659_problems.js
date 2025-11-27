3fe71f8c8e8925054e1455b0770600969f766efc
Alex <alexandriaduell@gmail.com>
2025-11-26 19:02:39 -0500

    Updated operation col to be one of an array of strings

--- db/migrations/20251126230659_problems.js
+++ db/migrations/20251126230659_problems.js
@@ -2,7 +2,7 @@ exports.up = async function (knex) {
     return knex.schema.createTable('problems', (tbl) => {
         tbl.uuid('problem_id').unique().primary()
         tbl.timestamp('created_at').defaultTo(knex.fn.now())
-        tbl.string('operation')
+        tbl.enu('operation', ['add', 'subtract', 'multiply', 'divide'])
         tbl.jsonb('operands')
         tbl.jsonb('solution')
         tbl.enu('difficulty', ['easy', 'medium', 'hard'])
